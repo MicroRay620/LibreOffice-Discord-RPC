e2d1564bd11bbfb1a05b65646b6672be614ac60f
MicroRay620 <microray620@gmail.com>
2025-10-21 23:19:35 -0400

    Created

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+// Discord RPC
+#include "discord_rpc.h"
+#include "discord_register.h"
+// LibreOffice UNO API
+#include <cppuhelper/bootstrap.hxx>
+#include <com/sun/star/frame/XComponentLoader.hpp>
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+/*
+ * Documentation will be added as the project expands. Right now it is in planning stages
+ * What this extension will do is access the Discord API to use the RPC. Similar to the Neovim plugin "presence.nvim" by andweeb
+ * Would prefer making this in Rust tbh
+*/
+int main() {
+    return 0;
+}
+
